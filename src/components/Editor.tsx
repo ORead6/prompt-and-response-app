2229f925d7cb812dada00a18b8ca4104f89c86dc
ORead6 <owen-read@outlook.com>
2025-03-04 01:07:29 +0000

    LoadState + Toolbar

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,117 +1,120 @@
-import { $getRoot, $getSelection } from 'lexical';
-import { useEffect } from 'react';
+"use client";
 
-import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
-import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { ContentEditable } from '@lexical/react/LexicalContentEditable';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
-import Toolbars from './Toolbars';
-
-// Style each with Tailwind CSS
-const theme = {
-  ltr: 'ltr',
-  rtl: 'rtl',
-  paragraph: '',
-  quote: 'editor-quote',
-  heading: {
-    h1: 'editor-heading-h1',
-    h2: 'editor-heading-h2',
-    h3: 'editor-heading-h3',
-    h4: 'editor-heading-h4',
-    h5: 'editor-heading-h5',
-    h6: 'editor-heading-h6',
-  },
-  list: {
-    nested: {
-      listitem: 'editor-nested-listitem',
-    },
-    ol: 'editor-list-ol',
-    ul: 'editor-list-ul',
-    listitem: 'editor-listItem',
-    listitemChecked: 'editor-listItemChecked',
-    listitemUnchecked: 'editor-listItemUnchecked',
-  },
-  hashtag: 'editor-hashtag',
-  image: 'editor-image',
-  link: 'editor-link',
-  text: {
-    bold: 'editor-textBold',
-    code: 'editor-textCode',
-    italic: 'editor-textItalic',
-    strikethrough: 'editor-textStrikethrough',
-    subscript: 'editor-textSubscript',
-    superscript: 'editor-textSuperscript',
-    underline: 'editor-textUnderline',
-    underlineStrikethrough: 'editor-textUnderlineStrikethrough',
-  },
-  code: 'editor-code',
-  codeHighlight: {
-    atrule: 'editor-tokenAttr',
-    attr: 'editor-tokenAttr',
-    boolean: 'editor-tokenProperty',
-    builtin: 'editor-tokenSelector',
-    cdata: 'editor-tokenComment',
-    char: 'editor-tokenSelector',
-    class: 'editor-tokenFunction',
-    'class-name': 'editor-tokenFunction',
-    comment: 'editor-tokenComment',
-    constant: 'editor-tokenProperty',
-    deleted: 'editor-tokenProperty',
-    doctype: 'editor-tokenComment',
-    entity: 'editor-tokenOperator',
-    function: 'editor-tokenFunction',
-    important: 'editor-tokenVariable',
-    inserted: 'editor-tokenSelector',
-    keyword: 'editor-tokenAttr',
-    namespace: 'editor-tokenVariable',
-    number: 'editor-tokenProperty',
-    operator: 'editor-tokenOperator',
-    prolog: 'editor-tokenComment',
-    property: 'editor-tokenProperty',
-    punctuation: 'editor-tokenPunctuation',
-    regex: 'editor-tokenVariable',
-    selector: 'editor-tokenSelector',
-    string: 'editor-tokenSelector',
-    symbol: 'editor-tokenProperty',
-    tag: 'editor-tokenProperty',
-    url: 'editor-tokenOperator',
-    variable: 'editor-tokenVariable',
-  },
-};
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
+import Toolbars from "./Toolbars";
+import { HeadingNode } from "@lexical/rich-text";
+import LoadState from "./loadState";
+import { ListNode, ListItemNode } from "@lexical/list";
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
 // try to recover gracefully without losing user data.
-function onError(error: any) {
-  console.error(error);
+function onError(error: unknown) {
+	console.error(error);
 }
 
 export default function Editor() {
-  const initialConfig = {
-    namespace: 'ResponseEditor',
-    theme,
-    onError,
-  };
+	const initialConfig = {
+		namespace: "MyEditor",
+		theme: theme,
+		onError,
+		nodes: [HeadingNode, ListNode, ListItemNode],
+		editable: false,
+	};
 
-  return (
-    <div className='max-w-4xl mx-auto rounded-lg h-[50vh] p-5'>
-      <LexicalComposer initialConfig={initialConfig}>
-        <Toolbars />
-        <RichTextPlugin
-          contentEditable={
-            <ContentEditable
-            className='focus:outline-none'
-              aria-placeholder={'Enter some text...'}
-              placeholder={<div>Enter some text...</div>}
-            />
-          }
-          ErrorBoundary={LexicalErrorBoundary}
-        />
-        <HistoryPlugin />
-        <AutoFocusPlugin />
-      </LexicalComposer>
-    </div>
-  );
+	return (
+		<div className="max-w-4xl mx-auto bg-none border rounded-lg h-[50vh] p-5">
+			<LexicalComposer initialConfig={initialConfig}>
+				<LoadState />
+				<Toolbars />
+        <ListPlugin />
+				<RichTextPlugin
+					contentEditable={
+						<ContentEditable className=" focus:outline-none" />
+					}
+					placeholder={<div>Enter some text...</div>}
+					ErrorBoundary={LexicalErrorBoundary}
+				/>
+				<HistoryPlugin />
+				<AutoFocusPlugin />
+			</LexicalComposer>
+		</div>
+	);
 }
+
+const theme = {
+	ltr: "ltr",
+	rtl: "rtl",
+	paragraph: "",
+	quote: "editor-quote",
+	heading: {
+		h1: "text-3xl font-bold",
+		h2: "editor-heading-h2",
+		h3: "editor-heading-h3",
+		h4: "editor-heading-h4",
+		h5: "editor-heading-h5",
+		h6: "editor-heading-h6",
+	},
+	list: {
+		nested: {
+			listitem: "editor-nested-listitem",
+		},
+		ol: "list-decimal",
+		ul: "list-disc",
+		listitem: "editor-listItem",
+		listitemChecked: "editor-listItemChecked",
+		listitemUnchecked: "editor-listItemUnchecked",
+	},
+	hashtag: "editor-hashtag",
+	image: "editor-image",
+	link: "editor-link",
+	text: {
+		bold: "font-bold",
+		code: "editor-textCode",
+		italic: "italic",
+		strikethrough: "line-through",
+		subscript: "editor-textSubscript",
+		superscript: "editor-textSuperscript",
+		underline: "underline",
+		underlineStrikethrough: "editor-textUnderlineStrikethrough",
+	},
+	code: "editor-code",
+	codeHighlight: {
+		atrule: "editor-tokenAttr",
+		attr: "editor-tokenAttr",
+		boolean: "editor-tokenProperty",
+		builtin: "editor-tokenSelector",
+		cdata: "editor-tokenComment",
+		char: "editor-tokenSelector",
+		class: "editor-tokenFunction",
+		"class-name": "editor-tokenFunction",
+		comment: "editor-tokenComment",
+		constant: "editor-tokenProperty",
+		deleted: "editor-tokenProperty",
+		doctype: "editor-tokenComment",
+		entity: "editor-tokenOperator",
+		function: "editor-tokenFunction",
+		important: "editor-tokenVariable",
+		inserted: "editor-tokenSelector",
+		keyword: "editor-tokenAttr",
+		namespace: "editor-tokenVariable",
+		number: "editor-tokenProperty",
+		operator: "editor-tokenOperator",
+		prolog: "editor-tokenComment",
+		property: "editor-tokenProperty",
+		punctuation: "editor-tokenPunctuation",
+		regex: "editor-tokenVariable",
+		selector: "editor-tokenSelector",
+		string: "editor-tokenSelector",
+		symbol: "editor-tokenProperty",
+		tag: "editor-tokenProperty",
+		url: "editor-tokenOperator",
+		variable: "editor-tokenVariable",
+	},
+};
--- src/components/Toolbars.tsx
+++ src/components/Toolbars.tsx
@@ -1,15 +1,211 @@
-import React from 'react';
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { FORMAT_TEXT_COMMAND } from 'lexical';
+"use client";
+
+import React, { useCallback } from "react";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import {
+  $getSelection,
+  $isRangeSelection,
+  CAN_REDO_COMMAND,
+  CAN_UNDO_COMMAND,
+  FORMAT_TEXT_COMMAND,
+  REDO_COMMAND,
+  UNDO_COMMAND,
+} from "lexical";
+import { useState, useEffect } from "react";
+import { mergeRegister } from "@lexical/utils";
+import { Redo, Undo } from "lucide-react";
+import { useDebouncedCallback } from "use-debounce";
+import { $createHeadingNode, $isHeadingNode } from "@lexical/rich-text";
+import { $setBlocksType } from "@lexical/selection";
+import { $isListNode, INSERT_ORDERED_LIST_COMMAND, INSERT_UNORDERED_LIST_COMMAND, REMOVE_LIST_COMMAND } from "@lexical/list";
+
+const LowPriority = 1;
 
 const Toolbars = () => {
-    const [editor] = useLexicalComposerContext();
+  const [editor] = useLexicalComposerContext();
+  const [isBold, setIsBold] = useState(false);
+  const [isItalic, setIsItalic] = useState(false);
+  const [isUnderline, setIsUnderline] = useState(false);
+  const [isStrikeThrough, setIsStrikeThrough] = useState(false);
+  const [isOrderedList, setIsOrderedList] = useState(false);
+  const [isHeading1, setIsHeading1] = useState(false);
 
-    return (
-        <div><button onClick={() => {
-            editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold');
-        }}>B</button></div>
+  const [canUndo, setCanUndo] = useState(false);
+  const [canRedo, setCanRedo] = useState(false);
+
+  const $updateToolbar = useCallback(() => {
+    const selection = $getSelection();
+
+    if ($isRangeSelection(selection)) {
+      setIsBold(selection.hasFormat("bold"));
+      setIsItalic(selection.hasFormat("italic"));
+      setIsUnderline(selection.hasFormat("underline"));
+      setIsStrikeThrough(selection.hasFormat("strikethrough"));
+
+      // Get the parent node of the selection to check for block level formatting
+      const anchorNode = selection.anchor.getNode();
+      const element =
+        anchorNode.getKey() === "root"
+          ? anchorNode
+          : anchorNode.getTopLevelElementOrThrow();
+
+      // Check if element is part of a list
+      const parentList = $isListNode(element)
+        ? element
+        : element.getParentOrThrow();
+      setIsOrderedList(
+        $isListNode(parentList) && parentList.getListType() === "number"
+      );
+
+      // Check if element is a heading
+      setIsHeading1(
+        element ? $isHeadingNode(element) && element.getTag() === "h1" : false
+      );
+    }
+  }, [editor]);
+
+//   Save Content when user stops typing after 500ms
+  const handleSave = useDebouncedCallback((content) => {
+    console.log(content);
+  }, 500);
+
+//   Change styling of text on button presses
+  useEffect(() => {
+    mergeRegister(
+      editor.registerUpdateListener(
+        ({ editorState, dirtyElements, dirtyLeaves }) => {
+          editorState.read(() => {
+            $updateToolbar();
+          });
+
+          if (dirtyElements.size === 0 && dirtyLeaves.size === 0) {
+            return;
+          }
+          handleSave(JSON.stringify(editorState));
+        }
+      ),
+      editor.registerCommand(
+        CAN_UNDO_COMMAND,
+        (payload) => {
+          setCanUndo(payload);
+          return false;
+        },
+        LowPriority
+      ),
+      editor.registerCommand(
+        CAN_REDO_COMMAND,
+        (payload) => {
+          setCanRedo(payload);
+          return false;
+        },
+        LowPriority
+      )
     );
-}
+  }, [editor, $updateToolbar]);
+
+  const handleHeading = () => {
+    editor.update(() => {
+      const selection = $getSelection();
+      if ($isRangeSelection(selection)) {
+        $setBlocksType(selection, () => $createHeadingNode("h1"));
+      }
+    });
+  };
+
+//   Create Ordered List
+  const handleOLButton = () => {
+    editor.update(() => {
+      const selection = $getSelection();
+      if ($isRangeSelection(selection)) {
+        if (isOrderedList) {
+            editor.dispatchCommand(REMOVE_LIST_COMMAND, undefined);
+            setIsOrderedList(true);
+        }
+        else {
+            editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND, undefined);
+            setIsOrderedList(false);
+        }
+      }
+    });
+  };
+
+  if (!editor._editable) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-row space-x-3">
+      <button
+        onClick={() => {
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
+        }}
+        className={`size-8 rounded flex items-center justify-center ${
+          isBold ? "bg-gray-200" : ""
+        }`}
+      >
+        B
+      </button>
+      <button
+        onClick={() => {
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
+        }}
+        className={`size-8 italic rounded flex items-center justify-center ${
+          isItalic ? "bg-gray-200" : ""
+        }`}
+      >
+        i
+      </button>
+      <button
+        onClick={() => {
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "underline");
+        }}
+        className={`size-8 underline rounded flex items-center justify-center ${
+          isUnderline ? "bg-gray-200" : ""
+        }`}
+      >
+        U
+      </button>
+      <button
+        onClick={() => {
+          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "strikethrough");
+        }}
+        className={`size-8 line-through rounded flex items-center justify-center ${
+          isStrikeThrough ? "bg-gray-200" : ""
+        }`}
+      >
+        S
+      </button>
+      <button onClick={handleHeading} className={` size-8 rounded-md `}>
+        h1
+      </button>
+      <button
+        onClick={handleOLButton}
+        className={`size-8 rounded-md flex items-center justify-center ${
+          isOrderedList ? "bg-gray-200" : ""
+        }`}
+      >
+        UL
+      </button>
+      <button
+        disabled={!canUndo}
+        onClick={() => {
+          editor.dispatchCommand(UNDO_COMMAND, undefined);
+        }}
+        className="size-8 rounded flex items-center justify-center disabled:opacity-50"
+      >
+        <Undo width={20} />
+      </button>
+      <button
+        disabled={!canRedo}
+        onClick={() => {
+          editor.dispatchCommand(REDO_COMMAND, undefined);
+        }}
+        className="size-8 rounded flex items-center justify-center disabled:opacity-50"
+      >
+        <Redo width={20} />
+      </button>
+    </div>
+  );
+};
 
 export default Toolbars;
